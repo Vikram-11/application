8500631d755da8383c8579b7a2196743bca42bcc
admin1 <admi1@test.com>
2025-12-12 07:02:13 +0000

    Revert "calculator app"
    This reverts commit 29ae92293b4433c763b77185ad3f31bd5e894c03.

--- app.py
+++ app.py
@@ -1,36 +1,14 @@
-from flask import Flask, render_template, request
+from flask import Flask
+import os
 
 app = Flask(__name__)
 
-@app.route("/", methods=["GET", "POST"])
-def calculator():
-    result = ""
-    if request.method == "POST":
-        try:
-            num1 = float(request.form["num1"])
-            num2 = float(request.form["num2"])
-            operation = request.form["operation"]
+@app.route("/")
 
-            if operation == "add":
-                result = num1 + num2
-            elif operation == "subtract":
-                result = num1 - num2
-            elif operation == "multiply":
-                result = num1 * num2
-            elif operation == "divide":
-                if num2 != 0:
-                    result = num1 / num2
-                else:
-                    result = "Cannot divide by zero"
-            else:
-                result = "Invalid operation"
-        except ValueError:
-            result = "Invalid input"
-
-    return render_template("index.html", result=result)
+def hello():
+    return "<h1>Sample Python WebApp CI/CD demo!!!!!</h1>"
 
 if __name__ == "__main__":
-    app.run(debug=True)
-
+    
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
